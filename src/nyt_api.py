d0ef76b99903f39c7a277f69b979a9f278bd42c0
Ovidiu Sofariu <ovi@sofariu.com>
2025-01-23 13:48:19 -0500

    using vector index instead of string matching

--- src/nyt_api.py
+++ src/nyt_api.py
@@ -1,4 +1,5 @@
 from cache import NewsCache
+from index import Index
 from dataclasses import dataclass
 from typing import Literal, TypedDict, List
 import requests
@@ -15,6 +16,7 @@ logger = logging.getLogger(__name__)
 class ArchiveItem(TypedDict):
     """This class represents an item in the NYT Archive API response."""
 
+    archive_date: str
     pub_date: str
     headline: str
     abstract: str
@@ -29,9 +31,16 @@ class ArchiveResponse(TypedDict):
 
 
 class NYTApi:
-    def __init__(self, api_key: str, max_range: int = 12, cache: NewsCache = None):
+    def __init__(
+        self,
+        api_key: str,
+        max_range: int = 12,
+        cache: NewsCache = None,
+        index: Index = None,
+    ):
         self.api_key = api_key
         self.cache = cache or NewsCache()
+        self.index = index or Index()
         self.max_range = max_range
 
     def get_archives(
@@ -46,7 +55,7 @@ class NYTApi:
             end_date: the beginning of the date range with format YYYY.MM.
         """
 
-        months_to_query = self.generateMonths(start_date, end_date)
+        months_to_query = self.generate_months(start_date, end_date)
         if len(months_to_query) > self.max_range:
             month = "month" if self.max_range == 1 else "months"
             return {
@@ -55,28 +64,39 @@ class NYTApi:
                 "message": f"Your time range is too large. Choose a time range no longer than {self.max_range} {month}.",
             }
 
-        archiveItems = []
+        archive_items_by_month = []
         for year, month in months_to_query:
             api_response = self.get_monthly_archive(year, month)
-            archiveItems.extend(api_response)
+            archive_item_date = f"{year}-{month}"
+            self.index.create_vector_store(archive_item_date)
+            archive_items_by_month.append(api_response)
 
-        filtered_archive_items = self.filter_by_topic(archiveItems, topic)
+        filtered_archive_items = self.filter_by_topic(archive_items_by_month, topic)
 
         return {"status": "Ok", "responses": filtered_archive_items}
 
     def filter_by_topic(
-        self, archive_items: List[ArchiveItem], topic: str
+        self, archive_items_for_month: List[List[ArchiveItem]], topic: str
     ) -> List[ArchiveItem]:
         """This function filters the archive items by a given topic."""
         if topic == None:
-            return archive_items
+            return [
+                archive_item
+                for archive_item in archive_items_for_month
+                for archive_items_by_month in archive_items_by_month
+            ]
 
-        return [
-            item
-            for item in archive_items
-            if topic.lower() in item.get("headline", "").lower()
-            or topic.lower() in item.get("abstract", "").lower()
-        ]
+        response = []
+        for archive_items_by_month in archive_items_for_month:
+            if len(archive_items_by_month) > 0:
+                archive_date = archive_items_by_month[0].get("archive_date")
+                matched_items = self.index.search_index(archive_date, topic)
+
+                for archive_item in archive_items_by_month:
+                    headline = archive_item.get("headline")
+                    if archive_item.get("headline") in matched_items:
+                        response.append(archive_item)
+        return response
 
     def get_monthly_archive(self, year, month) -> List[ArchiveItem]:
         """get archive from cache if present, else call the API and cache the results
@@ -85,34 +105,34 @@ class NYTApi:
             month (_type_): _description_
         """
         if cached_archive := self.cache.get_by_date(year, month):
-            logger.debug(f"Using cached archive for {year}-{month}")
+            logger.info(f"Using cached archive for {year}-{month}")
             return cached_archive
         archive = self.call_archive_api(year, month)
         self.cache.put_by_date(year, month, archive)
         return archive
 
-    def call_archive_api(self, year, month) -> List[ArchiveItem]:
+    def call_archive_api(self, year: str, month: str) -> List[ArchiveItem]:
         """This function returns the NYT Archive API response for a given year and month."""
-        year = int(year)
-        month = int(month)
         base_url = "https://api.nytimes.com/svc/archive/v1"
-        url = f"{base_url}/{year}/{month}.json?api-key={self.api_key}"
+        url = f"{base_url}/{int(year)}/{int(month)}.json?api-key={self.api_key}"
 
-        logger.debug(f"Calling NYT Archive API for {year}-{month}")
+        self.ogger.info(f"Calling NYT Archive API for {year}-{month}")
         response = requests.get(url)
         status_code = response.status_code
         if status_code != 200:
+            logger.error(f"API error: {response.text}")
             raise (Exception(f"Error: {response.text}"))
         json_response = response.json()
-        return self.map_to_archive_item(json_response)
+        return self.map_to_archive_item(json_response, f"{year}-{month}")
 
-    def map_to_archive_item(self, api_response) -> List[ArchiveItem]:
+    def map_to_archive_item(self, api_response, archive_date) -> List[ArchiveItem]:
         """This function maps each doc in the API response to an ArchiveItem."""
         docs = api_response.get("response", {}).get("docs", [])
         if not docs:
             return []
         return [
             ArchiveItem(
+                archive_date=archive_date,
                 pub_date=doc.get("pub_date", ""),
                 headline=doc.get("headline", {}).get("main", ""),
                 abstract=doc.get("abstract", ""),
@@ -122,7 +142,7 @@ class NYTApi:
             for doc in docs
         ]
 
-    def generateMonths(self, start_date: str, end_date: str) -> List[str]:
+    def generate_months(self, start_date: str, end_date: str) -> List[str]:
         """Given the range of months, generate a list of (year, month) tuples
         to make it easy to call the NYT Archive API for each month in the range.
 
@@ -149,7 +169,7 @@ class NYTApi:
 
 
 def main():
-    logging.basicConfig(filename="logs/nyt_api.log", level=logging.DEBUG)
+    logging.basicConfig(filename="logs/nyt_api.log", level=logging.INFO)
 
     today = datetime.today().strftime("%Y-%m-%d")
     three_months_ago = np.datetime64(today) - np.timedelta64(90, "D")
